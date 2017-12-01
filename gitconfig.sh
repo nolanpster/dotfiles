dafb26237eb44dd85c3104bb2964aebf7f4ca682
Nolan Poulin <npoulin@mitre.org>
2017-12-01 17:57:28 +0000

    Added lgp, cf, ria aliases.

--- gitconfig.sh
+++ gitconfig.sh
@@ -8,3 +8,6 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.lg 'log --decorate --graph'
 git config --global alias.unstage 'reset HEAD --'
+git config --global alias.cf = "!f() { git commit --fixup=\"$1\"; }; f"
+git config --global alias.ria = "!f() { git rebase -i --autosquash HEAD~\"$1\"; }; f"
+git config --global alias.lgp = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all
